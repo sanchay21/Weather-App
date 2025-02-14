c716b79731307ba59d7d0ba0344af4da714d55ad
Sanchay Singh <sanchaysingh21@gmail.com>
2025-02-14 18:05:55 +0530

    Final Commit

--- scripts.js
+++ scripts.js
@@ -1,29 +1,21 @@
 const key = "fa1c25f1a516495aa75132832251302";
 
-document.getElementById("city").addEventListener("input", () => {
-    query = this.value;
-    
-    async function autocomplete() {
-        let suggestions = await fetch(`https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${query}`);
-        sugg_data = await suggestions.json()
-        console.log(sugg_data);
-    }
-    
-})
-
 async function getData() {
     const city = document.getElementById('city').value;
 
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`);
     const data = await response.json();
 
+    console.log(data)
+
     const temp = data.current.temp_c;
+    const city_name = data.location.name;
     const condition = data.current.condition.text;
     const icon = data.current.condition.icon;
     
     document.querySelector('.weather-box').innerHTML = `
-        <p>${temp}</p>
-        <p>${condition}</p>
-        <img src="https:${icon}" alt="Weather icon">
+        <p style="font-size:54px;">${city_name} </p>
+        <img src="https:${icon}" style="width:100px; height:100px;" alt="Weather icon">
+        <p style="font-size:54px;">${temp}°c</p>
     `;
 }
